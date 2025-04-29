3975985d24106e096d89033fb42b2886c4e81c76
Nadhil-an <nadhil.edu@gmail.com>
2025-04-30 02:41:54 +0530

    implement produc quantity  increment and decreament functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import CartItem,Cart
 from store.models import Product
 from django.http import HttpResponse
@@ -39,6 +39,17 @@ def add_cart(request,product_id):
         )
         cart_item.save()
 
+    return redirect('cart')
+
+def remove_item(request,product_id):
+    cart = Cart.objects.get(cart_id=__cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product,cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -=1
+        cart_item.save()
+    else:
+        cart_item.delete()
     return redirect('cart')
 
 
