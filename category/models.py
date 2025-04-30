596f0feef917c307c9bd5d3bda7a920cbc7c9824
Nadhil-an <nadhil.edu@gmail.com>
2025-04-30 23:02:19 +0530

    set the navbar cart quantity count

--- category/models.py
+++ category/models.py
@@ -5,10 +5,10 @@ from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=500, unique=True)
-    description = models.TextField(max_length=100)
-    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+    category_name  = models.CharField(max_length=100, unique=True)
+    slug           = models.SlugField(max_length=500, unique=True)
+    description    = models.TextField(max_length=100)
+    cat_image      = models.ImageField(upload_to='photos/categories', blank=True)
     
     class Meta:
         verbose_name = 'category'
