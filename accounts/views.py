e54d73c5f6c45adff2532c6ea831f4af68a30100
Nadhil-an <nadhil.edu@gmail.com>
2025-05-03 01:03:30 +0530

    implementing email verification for user registration

--- accounts/views.py
+++ accounts/views.py
@@ -22,6 +22,20 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
             user.phone_number = phone_number
             user.save()
+
+            #activation required
+            current_site = get_current_site(request)
+            mail_subject = 'please activate your account'
+            message      = rendr_to_string('account_activation_email.html',{
+                'user'   :user,
+                'domain' :current_site,
+                'uid'    :urlsafe_base64_encode(force_bytes(user.pk)),
+                'token'  :default_token_generator.make_token(user),
+            })
+
+            to_email    = email
+            send_email  = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
             messages.success(request,'Registration Successfull')
             return redirect('register')
     else:
