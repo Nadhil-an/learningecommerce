1793d4bc0c7003d783a988254ee364e2e68cb9cb
Nadhil-an <nadhil.edu@gmail.com>
2025-05-02 23:04:38 +0530

    implement django message and login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect
 from . forms import RegistrationForm
 from .models import Account
+from django.contrib import auth,messages
 
 # Create your views here.
 
+
+## user register functionality
 def register(request):
     if request.method == 'POST':
         form=RegistrationForm(request.POST)
@@ -17,18 +20,36 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
             user.phone_number = phone_number
             user.save()
+            messages.success(request,'Registration Successfull')
             return redirect('register')
     else:
         form = RegistrationForm()
 
-    
     context = {
         'form' : form,
         }
     return render(request,'register.html',context)
 
+
+
+### login functionality
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email,password=password)
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,'login Successfull')
+            return redirect('home')
+        else:
+            messages.error(request,'Invalid login credentials')
+            return redirect('login')
     return render(request,'login.html')
+
+
+
 
 def logout(request):
     return render(render,'logout.html')
