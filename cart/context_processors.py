aab95c60cda5adea682179e8ffdad40511d26166
Nadhil-an <nadhil.edu@gmail.com>
2025-05-13 09:33:39 +0530

    update cart counter

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -8,7 +8,10 @@ def counter(request):
     else:
         try:
             cart = Cart.objects.filter(cart_id=__cart_id(request))
-            cart_item = CartItem.objects.all().filter(cart=cart[:1])
+            if request.user.is_authenticated:
+                cart_item = CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_item = CartItem.objects.all().filter(cart=cart[:1])
             for cart_quantity in cart_item:
                 cart_count += cart_quantity.quantity
         except Cart.DoesNotExist:
